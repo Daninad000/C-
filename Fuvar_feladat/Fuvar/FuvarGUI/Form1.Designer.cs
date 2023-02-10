18937cdd46b43b21813837fe7b32de51242250df
Szabó Dániel <daniel.szabo.000@gmail.com>
2023-02-10 19:44:07 +0100

    Add files via upload

---
+++ Fuvar_feladat/Fuvar/FuvarGUI/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace FuvarGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbTaxiId = new System.Windows.Forms.Label();
+            this.textBoxTaxiId = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Fuvarok = new System.Windows.Forms.ListBox();
+            this.lbTaxiAdatok = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbTaxiId
+            // 
+            this.lbTaxiId.AutoSize = true;
+            this.lbTaxiId.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lbTaxiId.Location = new System.Drawing.Point(55, 97);
+            this.lbTaxiId.Name = "lbTaxiId";
+            this.lbTaxiId.Size = new System.Drawing.Size(373, 29);
+            this.lbTaxiId.TabIndex = 0;
+            this.lbTaxiId.Text = "Kérem adja meg a taxi sorszámát:";
+            // 
+            // textBoxTaxiId
+            // 
+            this.textBoxTaxiId.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxTaxiId.Location = new System.Drawing.Point(445, 99);
+            this.textBoxTaxiId.Name = "textBoxTaxiId";
+            this.textBoxTaxiId.Size = new System.Drawing.Size(100, 30);
+            this.textBoxTaxiId.TabIndex = 1;
+            this.textBoxTaxiId.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxTaxiId_KeyDown);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(599, 99);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 39);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Fuvarok
+            // 
+            this.Fuvarok.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Fuvarok.FormattingEnabled = true;
+            this.Fuvarok.ItemHeight = 29;
+            this.Fuvarok.Location = new System.Drawing.Point(283, 229);
+            this.Fuvarok.Name = "Fuvarok";
+            this.Fuvarok.Size = new System.Drawing.Size(408, 352);
+            this.Fuvarok.TabIndex = 4;
+            // 
+            // lbTaxiAdatok
+            // 
+            this.lbTaxiAdatok.AutoSize = true;
+            this.lbTaxiAdatok.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lbTaxiAdatok.Location = new System.Drawing.Point(345, 183);
+            this.lbTaxiAdatok.Name = "lbTaxiAdatok";
+            this.lbTaxiAdatok.Size = new System.Drawing.Size(287, 29);
+            this.lbTaxiAdatok.TabIndex = 5;
+            this.lbTaxiAdatok.Text = "A taxishoz tartozó fuvarok:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(997, 658);
+            this.Controls.Add(this.lbTaxiAdatok);
+            this.Controls.Add(this.Fuvarok);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxTaxiId);
+            this.Controls.Add(this.lbTaxiId);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbTaxiId;
+        private System.Windows.Forms.TextBox textBoxTaxiId;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lbTaxiAdatok;
+        private System.Windows.Forms.ListBox Fuvarok;
+    }
+}
+
---
+++ Fuvar_feladat/Fuvar/FuvarGUI/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+//using Fuvar;
+
+namespace FuvarGUI
+{
+     
+    public partial class Form1 : Form
+    {
+        private static List<Fuvar.Fuvar> fuvarok = new List<Fuvar.Fuvar>();
+        private static int taxiId;
+        public Form1()
+        {
+            InitializeComponent();
+            fajlbeolvas();
+
+
+            
+        }
+
+        // private static List<string> sorElemList = new List<string>();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                taxiId = int.Parse(textBoxTaxiId.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Kérlek csak számot adj meg.");
+                // MessageBox.Show(hiba.Message);
+                // throw;
+            }
+
+            Fuvarok.Items.Clear();
+
+            for (int i = 0; i < fuvarok.Count; i++)
+            {
+                if (taxiId == fuvarok[i].TaxiAzonosito)
+                {
+                    Fuvarok.Items.Add("Fuvar azonosítója: " + fuvarok[i].TaxiAzonosito.ToString());
+                    Fuvarok.Items.Add("Indulás idő: " + fuvarok[i].IndulásIdo.ToString());
+                    Fuvarok.Items.Add("Utazás idő: " + fuvarok[i].UtazasIdo.ToString());
+                    Fuvarok.Items.Add("Megtett táv: " + fuvarok[i].MegtettTav.ToString());
+                    Fuvarok.Items.Add("Viteldíj: " + fuvarok[i].VitelDij.ToString());
+                    Fuvarok.Items.Add("Borravaló: " + fuvarok[i].Borravalo.ToString());
+                    Fuvarok.Items.Add("Fizetés módja: " + fuvarok[i].FizetesMod.ToString());
+                }
+            }
+        }
+
+
+
+        private static void fajlbeolvas()
+        {
+            StreamReader beolvas = new StreamReader(@"fuvar.csv");
+            var fejlec = beolvas.ReadLine();
+            beolvas.ReadLine();
+            while (!beolvas.EndOfStream)
+            {
+                string sor = beolvas.ReadLine();
+                string[] sorElem = sor.Split(';');
+                fuvarok.Add(new Fuvar.Fuvar(int.Parse(sorElem[0]), sorElem[1], int.Parse(sorElem[2]), double.Parse(sorElem[3]), double.Parse(sorElem[4]), double.Parse(sorElem[5]), sorElem[6]));
+            }
+        }
+
+        private void textBoxTaxiId_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                try
+                {
+                    taxiId = int.Parse(textBoxTaxiId.Text);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Kérlek csak számot adj meg.");
+                    // MessageBox.Show(hiba.Message);
+                    // throw;
+                }
+
+                Fuvarok.Items.Clear();
+
+                for (int i = 0; i < fuvarok.Count; i++)
+                {
+                    if (taxiId == fuvarok[i].TaxiAzonosito)
+                    {
+                        Fuvarok.Items.Add("Fuvar azonosítója: " + fuvarok[i].TaxiAzonosito.ToString());
+                        Fuvarok.Items.Add("Indulás idő: " + fuvarok[i].IndulásIdo.ToString());
+                        Fuvarok.Items.Add("Utazás idő: " + fuvarok[i].UtazasIdo.ToString());
+                        Fuvarok.Items.Add("Megtett táv: " + fuvarok[i].MegtettTav.ToString());
+                        Fuvarok.Items.Add("Viteldíj: " + fuvarok[i].VitelDij.ToString());
+                        Fuvarok.Items.Add("Borravaló: " + fuvarok[i].Borravalo.ToString());
+                        Fuvarok.Items.Add("Fizetés módja: " + fuvarok[i].FizetesMod.ToString());
+                    }
+                }
+            }
+        }
+    }
+}
